eae20b2ba3f9d7103a13b3cb48822cb11ebfaa46
miller <miller@test.com>
2023-04-17 17:49:32 +0800

    完善注释

--- len-generator/src/main/java/com/len/generator/template/TemplateRenderer.java
+++ len-generator/src/main/java/com/len/generator/template/TemplateRenderer.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import com.len.generator.ErrConstant;
 import com.len.generator.metadata.MetaData;
 
+/**
+ * 模板 render类
+ */
 public class TemplateRenderer {
 
     protected TemplateStrategy templateStrategy;
@@ -32,11 +35,23 @@ public class TemplateRenderer {
 
         public Builder() {}
 
+        /**
+         * 根据模板对象渲染
+         * 
+         * @param templateStrategy 模板
+         * @return builder
+         */
         public Builder withTemplateStrategy(TemplateStrategy templateStrategy) {
             this.templateStrategy = templateStrategy;
             return this;
         }
 
+        /**
+         * 根据模板class渲染
+         * 
+         * @param templateStrategyClass 模板 class
+         * @return builder
+         */
         public Builder withTemplateStrategy(Class<? extends TemplateStrategy> templateStrategyClass) {
             try {
                 this.templateStrategy = templateStrategyClass.newInstance();
@@ -46,12 +61,23 @@ public class TemplateRenderer {
             return this;
         }
 
+        /**
+         * 构造一个模板 render
+         * 
+         * @return TemplateRenderer
+         */
         public TemplateRenderer build() {
             TemplateRenderer renderer = new TemplateRenderer();
             renderer.templateStrategy = templateStrategy;
             return renderer;
         }
 
+        /**
+         * 生成
+         * 
+         * @param data 元数据
+         * @return 生成的内容数组
+         */
         public String[] render(MetaData data) {
             if (templateStrategy == null) {
                 throw new RuntimeException(ErrConstant.ENGINE_NOT_CONFIG);
